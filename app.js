33e7f9014988784d904e0daace818326f726e2dc
Sachin Gupta <saching@microsoft.com>
2016-05-06 18:20:38 -0700

    adding bot to ask for name

--- app.js
+++ app.js
@@ -4,8 +4,22 @@ var builder = require('botbuilder');
 // Create bot and add dialogs
 var bot = new builder.BotConnectorBot({ appId: 'YourAppId', appSecret: 'YourAppSecret' });
 bot.add('/', function (session) {
-    session.send('Hello World');
+    if (!session.userData.name) {
+        session.beginDialog('/profile');
+    } else {
+        session.send('Hello %s!', session.userData.name);
+    }
 });
+
+bot.add('/profile', [
+    function (session) {
+        builder.Prompts.text(session, 'Hi! What is your name?');
+    },
+    function (session, results) {
+        session.userData.name = results.response;
+        session.endDialog();
+    }
+]);
 
 // Setup Restify Server
 var server = restify.createServer();
