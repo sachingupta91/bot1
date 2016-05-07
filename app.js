e0a376571250e9e72f95945f26444cbe10017eb5
Sachin Gupta <saching@microsoft.com>
2016-05-06 18:55:29 -0700

    adding bot to ask for play games guessmyname

--- app.js
+++ app.js
@@ -4,6 +4,60 @@ var builder = require('botbuilder');
 // Create bot and add dialogs
 var bot = new builder.BotConnectorBot({ appId: 'YourAppId', appSecret: 'YourAppSecret' });
 bot.add('/', new builder.CommandDialog()
+    .matches('^game', builder.DialogAction.beginDialog('/gameSelection'))
+    .matches('^playgame', builder.DialogAction.beginDialog('/gameSelection'))
+	.matches('^changegame', builder.DialogAction.beginDialog('/gameSelection'))
+    .matches('^quit', builder.DialogAction.endDialog())
+    .onDefault(function (session) {
+        if (!session.userData.currentSelectedGame) {
+            session.beginDialog('/gameSelection');
+        } else {
+            session.beginDialog('/guessMyName');
+        }
+    }));
+
+
+bot.add('/gameSelection',  [
+    function (session) {
+        if (session.userData.currentSelectedGame) {
+            builder.Prompts.text(session, 'What would you like to change it to?');
+        } else {
+            builder.Prompts.text(session, 'Hi! Do you want to play guess my name?');
+        }
+    },
+    function (session, results) {
+		if(results.response)
+		{
+           session.userData.currentSelectedGame = 'guessmyname';
+		}
+        session.endDialog();
+    }
+]);
+	
+bot.add('/guessMyName',  [
+    function (session) {
+        builder.Prompts.text(session, 'What is my name (Guess)?');
+    },
+    function (session, results) {
+		if(results.response === 'quit')
+		{
+			session.endDialog();
+			return;
+		}
+		if(results.response === 'saching')
+		{
+           session.send('You are correct. Now i recognize you..');
+		}
+		else{
+			session.send('Sorry!!! You are wrong. Try again..');
+		}
+        session.endDialog();
+		session.beginDialog('/guessMyName');
+    }
+]);
+
+/*
+bot.add('/', new builder.CommandDialog()
     .matches('^set name', builder.DialogAction.beginDialog('/profile'))
     .matches('^quit', builder.DialogAction.endDialog())
     .onDefault(function (session) {
@@ -26,7 +80,7 @@ bot.add('/profile',  [
         session.endDialog();
     }
 ]);
-
+*/
 
 // Setup Restify Server
 var server = restify.createServer();
