c95a29830ada59a59a30ca214ffbc8371805a3fd
Sachin Gupta <saching@microsoft.com>
2016-05-06 18:31:27 -0700

    adding bot to ask for gorup name and help to change it

--- app.js
+++ app.js
@@ -3,23 +3,30 @@ var builder = require('botbuilder');
 
 // Create bot and add dialogs
 var bot = new builder.BotConnectorBot({ appId: 'YourAppId', appSecret: 'YourAppSecret' });
-bot.add('/', function (session) {
-    if (!session.userData.name) {
-        session.beginDialog('/profile');
-    } else {
-        session.send('Hello %s!', session.userData.name);
-    }
-});
-
-bot.add('/profile', [
+bot.add('/', new builder.CommandDialog()
+    .matches('^set name', builder.DialogAction.beginDialog('/profile'))
+    .matches('^quit', builder.DialogAction.endDialog())
+    .onDefault(function (session) {
+        if (!session.userData.name) {
+            session.beginDialog('/profile');
+        } else {
+            session.send('Hello %s!', session.userData.name);
+        }
+    }));
+bot.add('/profile',  [
     function (session) {
-        builder.Prompts.text(session, 'Hi! What is your name?');
+        if (session.userData.name) {
+            builder.Prompts.text(session, 'What would you like to change it to?');
+        } else {
+            builder.Prompts.text(session, 'Hi! What is your project Group name?');
+        }
     },
     function (session, results) {
         session.userData.name = results.response;
         session.endDialog();
     }
 ]);
+
 
 // Setup Restify Server
 var server = restify.createServer();
